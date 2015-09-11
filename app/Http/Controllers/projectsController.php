9604cd1af2cb3e7eb480e6c74987104aa153f871
Volix <mc.awolski@gmail.com>
2015-09-11 23:51:15 +0200

    Convert ProjectsController to PSR-2

--- app/Http/Controllers/projectsController.php
+++ app/Http/Controllers/projectsController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use Image;
-use View;
-use App\Project;
 use App\Image as Photo;
+use App\Project;
+use Illuminate\Http\Request;
+use Image;
 use Input;
 use Redirect;
 use Session;
+use View;
 
 class projectsController extends Controller
 {
@@ -24,9 +22,9 @@ class projectsController extends Controller
     public function index()
     {
         //Return list of all projects
-        
+
         $projects = Project::all();
-        
+
     }
 
     /**
@@ -36,61 +34,59 @@ class projectsController extends Controller
      */
     public function create()
     {
-        $project =  new Project;
-        
+        $project = new Project;
+
         $project->name = Input::get('name');
-        
+
         $project->short_description = Input::get('short_description');
-        
+
         $project->description = strlen(Input::get('description')) > 0 ? Input::get('description') : Input::get('short_description');
-        
+
         $project->made_at = Input::get('made_at');
-        
+
         $project->project_url = Input::get('project_url');
-        
+
         $project->slug = str_slug(Input::get('name'));
-        
+
         $project_id = $project->save();
 
-		if (Input::hasFile('images')){
-			foreach (Input::file('images') as $image){
-				$date = date('Y-m-d');
-				$extension = $image->getClientOriginalExtension();
-				$filename = $image->getClientOriginalName();
-				
-				$image->move('uploads\\'.$date.'\\original\\original', $filename);
-				
-				$current_image = Image::make(public_path('uploads/'.$date.'/original/original/'.$filename));
-			
-				$width = $current_image->width();
-				$height = $current_image->height();
-				
-				if ($width >= 300 || $height >= 300) {
-					$destinationDir = public_path('content\\'.$date.'\\'.'300x300'.'\\original\\');
+        if (Input::hasFile('images')) {
+            foreach (Input::file('images') as $image) {
+                $date = date('Y-m-d');
+                $extension = $image->getClientOriginalExtension();
+                $filename = $image->getClientOriginalName();
 
-					if (!is_dir($destinationDir)) {
-						mkdir($destinationDir, 0777, true);
-					}
+                $image->move('uploads\\' . $date . '\\original\\original', $filename);
 
-					$current_image->resize(300, 300)->save($destinationDir.$filename);
-				}
-				
-			$photo = new Photo;
-			
-			$photo->name = $filename;
-			$photo->extension = $extension;
-			
-			$photo->save();
+                $current_image = Image::make(public_path('uploads/' . $date . '/original/original/' . $filename));
 
-			$project->images()->attach($photo->id);
-		
-	
-			}
-		}
-	
-    return Redirect::route('projectManageList')->with('communications_success', ["Dodano pomyślnie"]);
+                $width = $current_image->width();
+                $height = $current_image->height();
+
+                if ($width >= 300 || $height >= 300) {
+                    $destinationDir = public_path('content\\' . $date . '\\' . '300x300' . '\\original\\');
+
+                    if (!is_dir($destinationDir)) {
+                        mkdir($destinationDir, 0777, true);
+                    }
+
+                    $current_image->resize(300, 300)->save($destinationDir . $filename);
+                }
+
+                $photo = new Photo;
+
+                $photo->name = $filename;
+                $photo->extension = $extension;
+
+                $photo->save();
+
+                $project->images()->attach($photo->id);
+
+            }
+        }
+
+        return Redirect::route('projectManageList')->with('communications_success', ["Dodano pomyślnie"]);
     }
-    
 
     /**
      * Store a newly created resource in storage.
@@ -112,9 +108,9 @@ class projectsController extends Controller
     public function show($id)
     {
         //Return view with the project
-        
+
         return View::make('projectShow', ['project' => Project::find($id), 'images' => Project::find($id)->images()]);
-        
+
     }
 
     /**
@@ -128,7 +124,7 @@ class projectsController extends Controller
         $id = Project::where('slug', '=', $slug)->firstOrFail()['id'];
         return self::show($id);
     }
-    
+
     /**
      * Show the form for editing the specified resource.
      *
@@ -139,10 +135,10 @@ class projectsController extends Controller
     {
         //Find project by id
         $project = Project::find($id);
-        
-        if ($project){
+
+        if ($project) {
             return View::make('projectEdit', ['project' => $project]);
-        }else{
+        } else {
             abort(404);
         }
     }
@@ -157,35 +153,34 @@ class projectsController extends Controller
     public function update(Request $request, $id)
     {
         //update Project
-        
+
         $rules = array(
-            'name'       => 'required',
-            'made_at'      => 'required|date',
-            'project_url' => 'url'
+            'name' => 'required',
+            'made_at' => 'required|date',
+            'project_url' => 'url',
         );
-        $this -> validate($request, $rules);
+        $this->validate($request, $rules);
 
         $project = Project::find($id);
 
-        
         $project->name = Input::get('name');
-        
+
         $project->short_description = Input::get('short_description');
-        
+
         strlen(Input::get('description')) > 0 ? $project->description = Input::get('description') : $project->description = Input::get('short_description');
-        
+
         $project->made_at = Input::get('made_at');
-        
+
         $project->project_url = Input::get('project_url');
-        
+
         $project->slug = str_replace(' ', '_', Input::get('name'));
-        
+
         $project->save();
 
         // redirect
         Session::flash('message', 'Successfully updated nerd!');
         return View::make('projectsManageList', ['projects' => Project::all()]);
-        
+
     }
 
     /**
@@ -198,31 +193,31 @@ class projectsController extends Controller
     {
         //
         Project::destroy($id);
-        return    Redirect::route('projectManageList');
+        return Redirect::route('projectManageList');
     }
-    
+
     public function manage()
     {
         //Manage projects from dashboard
-        
+
         $projects = Project::all();
-        
-        if (count($projects)>0){
-        
+
+        if (count($projects) > 0) {
+
             return View::make('projectsManageList', ['projects' => Project::all()]);
-        
-        }else{
-        
+
+        } else {
+
             return Redirect::route('projectCreate')->with('communications_info', ["Nie odnaleziono żadnego projektu, dodaj jakiś"]);
-        
+
         }
-        
+
     }
-    
+
     private function images()
     {
         //load images for project
-        
+
         return $this->BelongsToMany('app/Image', 'projects_images');
     }
 }
